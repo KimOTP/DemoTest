b1ef2fcfad5b9032bdf0a2bd7a03165c8fe591b2
KimOPT <nguyenphongkien1512k@gmail.com>
2026-01-08 15:22:59 +0700

    Thang nao ngu

---
+++ src/main/java/com/giaolang/basis/Main.java
@@ -0,0 +1,12 @@
+package com.giaolang.basis;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main() {
+        System.out.println("Hello Maven and JDK-24");
+        System.out.println("Maven De Thuong vai");
+        System.out.println("to la 1 executable jar - to chay ngay tu dong lenh");
+        System.out.println("day la branch khac");
+    }
+}
